3d1a173e1d89e37b3119112ac1b82515190faf0b
Andrey Styskin <styskin@macbook-air-6.local>
2019-02-03 16:13:48 +0300

    Probability = 1

--- task2/main.cpp
+++ task2/main.cpp
@@ -15,9 +15,9 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     ifstream in;
-    in.open("/Users/styskin/bio2019/task2/task2/0.txt");
+    in.open("/Users/styskin/bio2019/task2/task2/1.txt");
     ofstream out;
-    out.open("/Users/styskin/bio2019/task2/task2/0.out");
+    out.open("/Users/styskin/bio2019/task2/task2/1.out");
     int T;
     in >> T;
     for (int t = 0; t < T; ++t) {
@@ -35,23 +35,29 @@ int main(int argc, const char * argv[]) {
         pp += tp;
         res += tp * 0.75;
         for (int i = 1; i <= k; ++i) {
+            // 0.214491825
+            // 0,034416825
             cnk = cnk * (k - i + 1) / i;
-            double p1 = pow(1.0 * (n) / L, i) * pow(1.0 * (L - n) / L, k - i);
-            pp += cnk * p1;
-            
-            // two states and choosen majority
-            // (i+1)/2 of i
+            double p1 = cnk * pow(1.0 * (n) / L, i) * pow(1.0 * (L - n) / L, k - i);
+            pp += p1;
+
+            double cnk2 = 1;
+            for (int j = 1; j < i / 2; ++j) {
+                cnk2 = cnk2 * (i - j + 1) / j;
+            }
             double px = 0;
-            if (i % 2 == 0) {
-                px += 0.5 * pow(p, i / 2) * pow(1 - p, i - i/2); // equal possibilities
+            if (i % 2 == 0) { // equal probabilities
+                cnk2 = cnk2 * (i - (i/2) + 1) / (i/2);
+                px += 0.5 * pow(p, i / 2) * pow(1 - p, i - i/2) * cnk2;
             }
             // C N K
             for (int j = i/2 + 1; j <= i; ++j) {
-                px += pow(p, j) * pow(1 - p, i - j); // all errors are errors
+                cnk2 = cnk2 * (i - j + 1) / j;
+                px += pow(p, j) * pow(1 - p, i - j) * cnk2; // all errors are errors
             }
-            res += cnk * p1 * px;
+            res += p1 * px;
         }
-        cout << res * L << " prob = "  << pp  << endl;
+        out << res * L << endl;
     }
     in.close();
     out.close();
