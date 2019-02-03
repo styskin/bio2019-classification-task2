5cb378f72a05892426d9fdf18147da8b15d08df6
Andrey Styskin <styskin@macbook-air-6.local>
2019-02-03 15:50:04 +0300

    C N K was added

--- task2/main.cpp
+++ task2/main.cpp
@@ -12,11 +12,12 @@
 #include <math.h>
 using namespace std;
 
+
 int main(int argc, const char * argv[]) {
     ifstream in;
-    in.open("/Users/styskin/bio2019/task2/task2/1.txt");
+    in.open("/Users/styskin/bio2019/task2/task2/0.txt");
     ofstream out;
-    out.open("/Users/styskin/bio2019/task2/task2/1.out");
+    out.open("/Users/styskin/bio2019/task2/task2/0.out");
     int T;
     in >> T;
     for (int t = 0; t < T; ++t) {
@@ -28,24 +29,29 @@ int main(int argc, const char * argv[]) {
         // p
         // k - number of reads
         double res = 0;
-        for (int i = 0; i < k; ++i) {
-            if (i == 0) {
-                res += pow(1.0 * (L - n) / L, k) * 0.75;
-            } else {
-                double p1 = pow(1.0 * (n) / L, i);
-                // two states and choosen majority
-                // (i+1)/2 of i
-                double px = 0;
-                if (i % 2 == 0) {
-                    px += 0.5 * pow(p, i / 2); // equal possibilities
-                }
-                for (int j = (i + 3)/2; j <= i; ++j) {
-                    px += pow(p, j); // all errors are errors
-                }
-                res += p1 * px;
+        double cnk = 1;
+        double pp = 0;
+        double tp = pow(1.0 * (L - n) / L, k);
+        pp += tp;
+        res += tp * 0.75;
+        for (int i = 1; i <= k; ++i) {
+            cnk = cnk * (k - i + 1) / i;
+            double p1 = pow(1.0 * (n) / L, i) * pow(1.0 * (L - n) / L, k - i);
+            pp += cnk * p1;
+            
+            // two states and choosen majority
+            // (i+1)/2 of i
+            double px = 0;
+            if (i % 2 == 0) {
+                px += 0.5 * pow(p, i / 2) * pow(1 - p, i - i/2); // equal possibilities
             }
+            // C N K
+            for (int j = i/2 + 1; j <= i; ++j) {
+                px += pow(p, j) * pow(1 - p, i - j); // all errors are errors
+            }
+            res += cnk * p1 * px;
         }
-        out << res * L << endl;
+        cout << res * L << " prob = "  << pp  << endl;
     }
     in.close();
     out.close();
